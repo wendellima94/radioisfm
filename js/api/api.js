c9c20847e68f9f89c3d0fb62f74e3ba7ca0e9fa8
wendellima94 <wendellima94>
2025-01-09 14:48:48 -0300

    social top bar

--- js/api/api.js
+++ js/api/api.js
@@ -12,8 +12,8 @@
 //       // Itera sobre os 6 primeiros posts recebidos da API
 //       data.posts.slice(0, 8).forEach((post) => {
 //         // Cria o HTML para cada post com classes personalizadas
-//         const blogHTML = ` 
-        
+//         const blogHTML = `
+
 //             <div class="col-lg-6 col-md-12 col-sm-12">
 //               <div class="custom-blog-card">
 //                 <img src="${post.imageUrl}" class="img-responsive" alt="Blog image" />
@@ -33,17 +33,16 @@
 //     .catch((error) => console.error("Error fetching data:", error));
 // });
 
-
 let currentImageIndex = 0;
-const images = document.querySelectorAll('.slider .prop-image');
+const images = document.querySelectorAll(".slider .prop-image");
 
 function showImage(index) {
   images.forEach((img, i) => {
-    img.classList.remove('active');
-    img.style.display = 'none';
+    img.classList.remove("active");
+    img.style.display = "none";
     if (i === index) {
-      img.classList.add('active');
-      img.style.display = 'block';
+      img.classList.add("active");
+      img.style.display = "block";
     }
   });
 }
@@ -60,25 +59,24 @@ function prevImage() {
 
 setInterval(nextImage, 5000);
 
-
-// carrousel programação 
-document.addEventListener('DOMContentLoaded', () => {
-  const sliderTrack = document.querySelector('.slider-track');
-  const prevBtn = document.querySelector('.prev-btn');
-  const nextBtn = document.querySelector('.next-btn');
+// carrousel programação
+document.addEventListener("DOMContentLoaded", () => {
+  const sliderTrack = document.querySelector(".slider-track");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
   const slides = sliderTrack.children.length;
   const visibleSlides = 4; // Quantidade de slides visíveis
   const slideWidth = 100 / slides; // Porcentagem da largura de um slide
   let currentIndex = 0;
 
-  prevBtn.addEventListener('click', () => {
+  prevBtn.addEventListener("click", () => {
     if (currentIndex > 0) {
       currentIndex--;
       updateSliderPosition();
     }
   });
 
-  nextBtn.addEventListener('click', () => {
+  nextBtn.addEventListener("click", () => {
     if (currentIndex < slides - visibleSlides) {
       currentIndex++;
       updateSliderPosition();
@@ -91,4 +89,16 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+// stick bar
+document.addEventListener("DOMContentLoaded", () => {
+  const stickyElement = document.getElementById("sticky-social-bar");
+  const stickyOffset = stickyElement.offsetTop;
 
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > stickyOffset) {
+      stickyElement.classList.add("sticky-bar");
+    } else {
+      stickyElement.classList.remove("sticky-bar");
+    }
+  });
+});
